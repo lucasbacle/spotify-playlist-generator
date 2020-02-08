8871adfb6dae4ec0a16a74830986455dc5d715a3
lucasbacle1 <lucas.bacle@free.fr>
2020-02-08 16:24:13 +0100

    add main classes

---
+++ app/App.py
@@ -0,0 +1,11 @@
+import SearchEngine
+import BluetoothManager
+import GraphicalUserInterface
+
+class App:
+
+    def __init__(self):
+        print("LOL")
+
+if __name__ == '__main__':
+    app = App()
---
+++ app/BluetoothManager.py
@@ -0,0 +1,4 @@
+class BluetoothManager:
+
+    def __init__(self):
+        pass
